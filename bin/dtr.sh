ffe454fd06031da7cae9f78714006c210b6d6b8b
Emir Turkes <eturkes@bu.edu>
2019-02-26 15:57:47 -0500

    Add OBS repo for Wine

--- bin/dtr.sh
+++ bin/dtr.sh
@@ -22,4 +22,5 @@ sudo zypper mr -d libdvdcss\ repository && i=1 \
     && sudo zypper mr -d google-chrome && i=$((i+1)) \
     && sudo zypper mr -d editors && i=$((i+1)) \
     && sudo zypper mr -d devel:languages:R:patched && i=$((i+1)) \
+    && sudo zypper mr -d Emulators:Wine && i=$((i+1)) \
     && echo ${i} repos disabled
--- bin/etr.sh
+++ bin/etr.sh
@@ -22,4 +22,5 @@ sudo zypper mr -e libdvdcss\ repository && i=1 \
     && sudo zypper mr -e google-chrome && i=$((i+1)) \
     && sudo zypper mr -e editors && i=$((i+1)) \
     && sudo zypper mr -e devel:languages:R:patched && i=$((i+1)) \
+    && sudo zypper mr -e Emulators:Wine && i=$((i+1)) \
     && echo ${i} repos enabled
