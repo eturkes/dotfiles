214c6d25951f888c233d6337d8942e6b9f1e4457
Emir Turkes <eturkes@bu.edu>
2019-08-17 06:53:41 +0100

    Remove R:patched repo

--- bin/dtr.sh
+++ bin/dtr.sh
@@ -21,7 +21,6 @@ sudo zypper mr -d libdvdcss\ repository && i=1 \
     && sudo zypper mr -d multimedia:apps && i=$((i+1)) \
     && sudo zypper mr -d spotify-easyrpm && i=$((i+1)) \
     && sudo zypper mr -d google-chrome && i=$((i+1)) \
-    && sudo zypper mr -d devel:languages:R:patched && i=$((i+1)) \
     && sudo zypper mr -d skype-stable && i=$((i+1)) \
     && sudo zypper mr -d network:im:signal && i=$((i+1)) \
     && echo ${i} repos disabled
