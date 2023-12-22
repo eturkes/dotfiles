14db953cad100c742743651fabb54e5a927d01b7
Emir Turkes <eturkes@bu.edu>
2023-12-22 10:55:10 +0000

    Deactivate utilities repo until bug is fixed

--- bin/etr.sh
+++ bin/etr.sh
@@ -26,7 +26,6 @@ sudo zypper mr -e spotify-easyrpm && i=$((i+1)) \
     && sudo zypper mr -e network:im:signal && i=$((i+1)) \
     && sudo zypper mr -e vscode && i=$((i+1)) \
     && sudo zypper mr -e nordvpn && i=$((i+1)) \
-    && sudo zypper mr -e utilities && i=$((i+1)) \
     && sudo zypper mr -e devel_tools && i=$((i+1)) \
     && sudo zypper mr -e games_tools && i=$((i+1)) \
     && sudo zypper mr -e filesystems && i=$((i+1)) \
